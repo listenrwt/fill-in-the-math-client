4872ba33e395a54cf55e1592e8d65ce6589b80b6
Jackson Chan <chanjackson0320@gmail.com>
2025-03-19 16:05:19 +0800

    Chore: Remove finished Todo

--- src/lib/game/utils.ts
+++ src/lib/game/utils.ts
@@ -27,23 +27,6 @@ const operator_difficulty_mapping = {
   // Hard: must contain multiplication or division, 1 - 2 blanks
 };
 
-// TODO: Implement the function generateMathQuestion and finish the Docstring (take lib/game/enum.ts as reference).
-
-// Description: Generate a math question based on the difficulty level.
-// The equation should not contain any negative numbers (but ok for intermediate results, e.g. ? - 4 + 3 = 1).
-// The equation answer should seat between 0 and 1000.
-// The blank answer must be a number between 0 - 9 and without replacement each other.
-// There should be only 1 blank or 1 number after MathSymbol.Equals.
-
-// Input: Difficulty
-// Easy: only addition and subtraction, only single digit numbers, 1 blank
-// Medium: only addition, subtraction, single and double digit numbers, at least one double digit number, 1 - 2 blanks
-//         or multiplication and division, single and double digit numbers, 1 blank
-// Hard: must contain multiplication or division, 1 - 2 blanks
-// Output: Array of numbers and math symbols
-
-// Example: generateMathQuestion(Difficulty.Easy) => [1, MathSymbol.Addition, MathSymbol.Blank, MathSymbol.Equals, 3]
-
 /**
  * generate math question randomly given difficulty
  *
