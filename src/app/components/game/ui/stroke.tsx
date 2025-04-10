428949b1bb5a447445db1f15012c5f48af25ee52
kamingkwok2015 <kwokkaming2015@gmail.com>
2025-04-10 15:10:50 +0800

    feat: added component stroke

---
+++ src/app/components/game/ui/stroke.tsx
@@ -0,0 +1,16 @@
+import { Divider } from '@mui/material';
+
+export default function GameContainer() {
+  return (
+    <Divider
+      sx={{
+        borderBottomWidth: 1.5,
+        borderColor: '#000000',
+        borderRadius: 1,
+        width: '90%',
+        alignContent: 'center',
+        margin: '0 auto',
+      }}
+    />
+  );
+}
