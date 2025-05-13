c4d4b2fb02b2c747a6feec27cc925837021b0467
listenrwt <listenrwtprivate@gmail.com>
2025-05-13 16:00:59 +0800

    fix: remove invalid avatar (id = 0)

--- src/app/assets/avatarMap.tsx
+++ src/app/assets/avatarMap.tsx
@@ -1,11 +1,11 @@
 const avatarMap: Record<number, string> = {
-  0: '/avatar/bear.png',
-  1: '/avatar/cat.png',
-  2: '/avatar/chicken.png',
-  3: '/avatar/elephant.png',
-  4: '/avatar/panda.png',
-  5: '/avatar/rabbit.png',
-  6: '/avatar/sealion.png',
+  1: '/avatar/bear.png',
+  2: '/avatar/cat.png',
+  3: '/avatar/chicken.png',
+  4: '/avatar/elephant.png',
+  5: '/avatar/panda.png',
+  6: '/avatar/rabbit.png',
+  7: '/avatar/sealion.png',
 };
 
 export default avatarMap;
